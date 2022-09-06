5051a213beabda1d8489f144232688d74b844158
Viktor <mannar@inbox.ru>
2022-09-06 07:04:30 +0500

    [5.2] Работа над списком истории записи

--- app/src/main/java/ru/burdin/clientbase/lits/ListHistoryAndRecordActivity.java
+++ app/src/main/java/ru/burdin/clientbase/lits/ListHistoryAndRecordActivity.java
@@ -1,6 +1,7 @@
 package ru.burdin.clientbase.lits;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.view.View;
@@ -9,10 +10,12 @@ import ru.burdin.clientbase.R;
 
 public class ListHistoryAndRecordActivity extends AppCompatActivity {
 
+    private RecyclerView recyclerView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_history_and_record);
+    recyclerView = findViewById(R.id.listHistory);
     }
 
     public void onClickButtonListHistoryAndRecordNewRecord(View view) {
