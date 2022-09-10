306a02b63fd3a4c06cd6eca1dd730e248c72ed1b
Viktor <mannar@inbox.ru>
2022-09-10 07:27:01 +0500

    [7.0] Добавлен поиск клиентов

--- app/src/main/java/ru/burdin/clientbase/lits/ListClientActivity.java
+++ app/src/main/java/ru/burdin/clientbase/lits/ListClientActivity.java
@@ -5,7 +5,10 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -14,6 +17,7 @@ import java.util.function.Consumer;
 import ru.burdin.clientbase.AddClientActivity;
 import ru.burdin.clientbase.AddSessionActivity;
 import ru.burdin.clientbase.Bd;
+import ru.burdin.clientbase.StaticClass;
 import ru.burdin.clientbase.cards.CardUserActivity;
 import ru.burdin.clientbase.MyAdapter;
 import ru.burdin.clientbase.R;
@@ -24,43 +28,110 @@ public class ListClientActivity extends AppCompatActivity {
     private Bd bd;
     private TextView textViewCount;
     private ArrayList <User> users;
+private EditText editTextSerch;
     private  String addSession = "";
+    private  Intent intent;
+private  RecyclerView recyclerViewClient;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_client);
-        Intent intent = new Intent(this, CardUserActivity.class);
+         intent = new Intent(this, CardUserActivity.class);
         bd = Bd.load(getApplicationContext());
         try {
             addSession = getIntent().getExtras().getString(AddSessionActivity.class.getName());
         } catch (Exception e) {
 
         }
-        RecyclerView recyclerViewClient = findViewById(R.id.list);
+ recyclerViewClient = findViewById(R.id.list);
         textViewCount = findViewById(R.id.textCountUsers);
-Consumer <MyAdapter.ViewHolder> consumer = viewHolder ->viewHolder.textView.setText(bd.getUsers().get(MyAdapter.count).getSurname() +  " " + bd.getUsers().get(MyAdapter.count).getName());
-MyAdapter myAdapter = new MyAdapter(this, bd.getUsers(), new MyAdapter.OnUserClickListener() {
-                    @Override
-                    public void onUserClick(Object o, int position) {
-                        if (addSession.equals(AddSessionActivity.class.getName())) {
-                            Intent intentAddSession = new Intent();
-                            intentAddSession.putExtra(ListClientActivity.class.getName(), position);
-                        setResult(RESULT_OK, intentAddSession);
-                        finish();
-                        }else {
-                            intent.putExtra(Bd.TABLE, position);
-                            startActivity(intent);
-                        }
-                        }
-}, consumer);
-        recyclerViewClient.setAdapter(myAdapter);
-        textViewCount.setText("Всего клиентов: " + bd.getUsers().size() + "");
-
+editTextSerch = findViewById(R.id.editTextListClientSearsh);
+users = bd.getUsers();
+listenerSearch();
+    updateList();
     }
 
+    /*
+    Запись нового клиента
+     */
     public  void  buttonAddClient (View view) {
         Intent intent = new Intent(this, AddClientActivity.class);
         startActivity(intent);
+    }
+
+    /*
+    Слушатель поиска
+     */
+    private  void listenerSearch () {
+    editTextSerch.addTextChangedListener(new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+if (charSequence.length() > 0) {
+users = new ArrayList<>();
+    String result = "";
+    if (charSequence.charAt(0) == '+' || charSequence.charAt(0) == '8') {
+        for (int j = 0; j < charSequence.length(); j++) {
+            if ((charSequence.charAt(j) == '+' && j == 0) || Character.isDigit(charSequence.charAt(j))) {
+                if (j == 0 && charSequence.charAt(j) == '8') {
+                    result = "+7";
+                    continue;
+                }
+                result = result + charSequence.charAt(j);
+            }
+        }
+        if (!result.equals(charSequence.toString())) {
+            editTextSerch.setText(result);
+        }
+        editTextSerch.setSelection(result.length());
+    }
+    for (User user : bd.getUsers()){
+        if (user.getSurname().toLowerCase().contains(charSequence) || user.getName().toLowerCase().contains(charSequence) || user.getPhone().contains(charSequence)
+        || user.getSurname().toUpperCase().contains(charSequence) || user.getName().toUpperCase().contains(charSequence)
+        ){
+users.add(user);
+    }
+}
+}else  {
+    users = bd.getUsers();
+}
+        updateList();
+        }
+
+        @Override
+        public void afterTextChanged(Editable editable) {
+
+        }
+    });
+    }
+
+    /*
+    Установка листа клиентов
+     */
+    private  void  updateList () {
+        Consumer <MyAdapter.ViewHolder> consumer = viewHolder ->viewHolder.textView.setText(users.get(MyAdapter.count).getSurname() +  " " + users.get(MyAdapter.count).getName());
+MyAdapter.OnUserClickListener <User> onUserClickListener = new MyAdapter.OnUserClickListener<User>() {
+    @Override
+    public void onUserClick(User user, int position) {
+        if (addSession.equals(AddSessionActivity.class.getName())) {
+            Intent intentAddSession = new Intent();
+            intentAddSession.putExtra(ListClientActivity.class.getName(), StaticClass.indexList(user.getId(), bd.getUsers()));
+            setResult(RESULT_OK, intentAddSession);
+            finish();
+        }else {
+            intent.putExtra(Bd.TABLE, StaticClass.indexList(user.getId(), bd.getUsers()));
+            startActivity(intent);
+        }
+    }
+};
+MyAdapter myAdapter = new MyAdapter(this, users , onUserClickListener, consumer);
+recyclerViewClient.setAdapter(myAdapter);
+textViewCount.setText("Всего клиентов: " + users.size() );
+
     }
 
 }
