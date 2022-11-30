77dd38ae8ccc3350c5f57e7d8f64d20701feee0e
Viktor <mannar@inbox.ru>
2022-11-30 10:53:10 +0500

    [16.3] Начало реализации sms

--- app/src/main/java/ru/burdin/clientbase/setting/TemplatesActivity.java
+++ app/src/main/java/ru/burdin/clientbase/setting/TemplatesActivity.java
@@ -2,6 +2,8 @@ package ru.burdin.clientbase.setting;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 
@@ -16,5 +18,16 @@ public class TemplatesActivity extends AppCompatActivity {
     }
 
     public void onClickTextViewTempletesSelectNotification(View view) {
+    }
+
+    public void onClickButtonTemplatesKey(View view) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+    builder.setItems(getResources().getStringArray(R.array.templetesKey), new DialogInterface.OnClickListener() {
+        @Override
+        public void onClick(DialogInterface dialogInterface, int i) {
+
+        }
+    });
+    builder.create().show();
     }
 }
